3c7ac7e9f85c0117bcf273df730883545b2d6485
saroj sundara <sarojkumarsundara518@gmail.com>
2022-10-17 01:07:12 +0530

    bring all comps

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import UserDashboard from '../src/components/UserDashboard'
+import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <UserDashboard />
+    </div>
+  )
+}
+
+export default App
